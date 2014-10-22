644ea979e3fead30af0e033740c3f1a4d6a53550
Vash-TS <nikitav4@gmail.com>
2014-10-22 14:55:09 -0400

    sample code

---
+++ test.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Oct 22 13:47:53 2014
+
+@author: Nikita
+"""
+import json
+import requests
+
+url= "http://www.giantbomb.com/api/character/3005-"
+url2 = "/?api_key=ce6972cdd7d9f713817345a0256f6bd645629481&format=json"
+
+print url + str(1) +url2
+
+for i in range(1,30):
+    data = requests.get(url+str(i)+url2).text
+    data = json.loads(data)
+    if not data['results']:
+        pass
+    else:
+        print i
+        print data['results']['name']
