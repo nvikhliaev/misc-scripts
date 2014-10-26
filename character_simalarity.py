33cd2f856bafa2ed2187454d629177db9f05ffbe
Vash-TS <nikitav4@gmail.com>
2014-10-25 22:25:38 -0400

    Update character_simalarity.py

--- character_simalarity.py
+++ character_simalarity.py
@@ -1,17 +1,16 @@
 """
-Uses the giantbomb api to produce a Jaccord simalarity index between two characters.
-To test it out, simply call simalarity_index(charID1,charID2), where charID1 and charID2 are
+Uses the giantbomb api to produce a Jaccord similarity index between two characters.
+To test it out, simply call similarity_index(charID1,charID2), where charID1 and charID2 are
 character IDs, as recorded by the giantbomb database.
 
 Example:
-simalarity_index(177,370)
+similarity_index(177,370)
 """
 import json
 import requests
 
 url= "http://www.giantbomb.com/api/character/3005-"
 url2 = "/?api_key=ce6972cdd7d9f713817345a0256f6bd645629481&format=json"
-
 
 class Character:
     def __init__(self, ID):
@@ -49,7 +48,7 @@ def jaccard(a,b):
     else:
         return float(len(intersect(a,b)))/float(len(union(a,b)))
      
-def simalarity_index(charID1,charID2):
+def similarity_index(charID1,charID2):
     char1 = Character(charID1)
     char2 = Character(charID2)
-    print "The simalarity index between "+char1.name()+" and " +char2.name()+" is "+str(jaccard(char1.concepts(), char2.concepts()))
+    print "The similarity index between "+char1.name()+" and " +char2.name()+" is "+str(jaccard(char1.concepts(), char2.concepts()))
